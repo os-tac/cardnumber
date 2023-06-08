da7f79add8f91093b0f0f16dfc0f5efde0ccc533
os-tac <127088505+os-tac@users.noreply.github.com>
2023-06-08 10:21:02 +0100

    Update creditCardNumber.js

--- creditCardNumber.js
+++ creditCardNumber.js
@@ -1,11 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
 
 export default class CreditCardNumber extends LightningElement {
+    @api cardNumber = "";
+
     onNumberChange(event){
         const enteredNumber = event.currentTarget.value;
         let enteredLoc = event.currentTarget.getAttribute("data-fieldname");
         if (enteredNumber.length == 4) {
+            this.cardNumber += event.currentTarget.value;
+            console.log(this.cardNumber);
             let nextBox = Number(enteredLoc)+1;
+            if (nextBox == 5) {
+                this.dispatchEvent(new FlowAttributeChangeEvent('cardNumber', this.cardNumber));
+            }
             let elem = this.template.querySelector("lightning-input[data-fieldname='"+nextBox+"']");
             setTimeout(() => elem.focus());
         }
